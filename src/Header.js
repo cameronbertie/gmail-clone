ac7ed6550f5c00ad5462c2874834a073447ee70a
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-10-17 18:32:33 +0100

    Built the header component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
   return (
     <div class="header">
-        <h2>This is a header</h2>
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png" alt="" />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret"/>
+      </div>
+      <div className="header__right">
+        <IconButton>
+        <AppsIcon />
+        </IconButton>
+        <IconButton>
+        <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
