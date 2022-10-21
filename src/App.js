01983e0dd13b1764e15d23213cc503cabd2110da
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-10-21 17:54:56 +0100

    Built the sidebar option component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { Router, Routes, Route } from "react-router-dom";
+import Mail from "./Mail";
+import EmailList from "./EmailList";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+        <Sidebar />
+        <Routes>
+          <Route path="/mail">
+            <Mail />
+          </Route>
+          <Route path="/">
+            <EmailList />
+          </Route>
+        </Routes>
+        </div>
+        
+      </div>
+    </Router>
   );
 }
 
